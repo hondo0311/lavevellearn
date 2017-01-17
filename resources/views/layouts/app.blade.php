74e2ba570412747f9232854c2c5ceb066203c3e6
lihanlu <lihanlu123@gmail.com>
2017-01-17 17:08:12 +0800

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel 快速入門 - 基本</title>
+
+        <!-- CSS 及 JavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar 內容 -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
